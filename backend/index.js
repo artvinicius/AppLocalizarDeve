1349d3123ded72f9612364b7dec3c95ca3a1a244
artvinicius <arthurviny1999@gmail.com>
2020-05-20 15:42:06 -0300

    Adicione métodos HTTP

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,22 @@ const express = require('express');
 
 const app = express();
 
-app.get('/',(request, response) => {
-return response.json({message: 'Hello Arthur'});
+
+app.use(express.json());
+
+// Métodos HTTP: GET,POST,PUT,DELETE
+
+//Tipos de parâmetros:
+
+//Query Params: request.query (Filtros, ordenação, paginação...)
+//Routr Params: request.params (Identificar um recurso na alteração ou remoção)
+//Body: request.body (Dados para a criação ou alteração de um regstro)
+
+app.post('/users', (request, response) => {
+    console.log(request.body);
+    return response.json({
+        message: 'Hello Arthur'
+    });
 });
 
 app.listen(3333);
