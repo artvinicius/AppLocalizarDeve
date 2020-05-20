ec5a95c05b1f36bc3db3ffc602dbe4dc8c7b0a3a
artvinicius <arthurviny1999@gmail.com>
2020-05-20 16:12:47 -0300

    Adicione configuração com o banco de dados

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,16 @@
 // Roda o servidor com node index.js
 // Adicionar dependencia para ele sempre que alterar alguma coisa do código ele atualizar = yarn add nodemon -D 
 // Rodar o servidor agora yarn nodemon index.js
+// Adicionando configurações com o banco de dados > yarn add mongoose
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
+mongoose.connect('mongodb+srv://Arthur:1234@cluster0-0jfxo.mongodb.net/LocDev?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 app.use(express.json());
 
@@ -17,6 +23,9 @@ app.use(express.json());
 //Query Params: request.query (Filtros, ordenação, paginação...)
 //Routr Params: request.params (Identificar um recurso na alteração ou remoção)
 //Body: request.body (Dados para a criação ou alteração de um regstro)
+
+
+// MongoDB (Não-relacional)
 
 app.post('/users', (request, response) => {
     console.log(request.body);
