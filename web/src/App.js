ebe57f0d82df426b05b645a1ba7fa24c4b4bb610
artvinicius <arthurviny1999@gmail.com>
2020-06-01 20:09:05 -0300

    Adicione Geolocation

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect} from 'react';
 
 import './global.css';
 import './App.css';
@@ -6,17 +6,32 @@ import './Sidebar.css';
 import './Main.css';
 
 function App() {
+  useEffect(() =>{
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      console.log(position); 
+      // const {latitude, longitude} = position.coords;
+    },
+    (err)=> {
+      console.log(err);
+    },
+    {
+      timeout: 30000,
+    }
+  );
+  }, []);
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
         <form>
-        <div class="input-block">
+        <div className="input-block">
           <label htmlFor="github_username">Usuário do Github </label>
           <input name="github_username" id="username_github" required></input>
         </div>
 
-        <div class="input-block">
+        <div className="input-block">
           <label htmlFor="techs">Tecnologias</label>
           <input name="techs" id="techs" required></input>
         </div>
@@ -27,7 +42,7 @@ function App() {
           <input name="latitude" id="latitude" required></input>
         </div>
 
-        <div class="input-block">
+        <div className="input-block">
           <label htmlFor="longitude">Longitude</label>
           <input name="longitude" id="longitude" required></input>
         </div>
