ef29121084d030d1a18c45e41947ad81b5f48d8c
artvinicius <arthurviny1999@gmail.com>
2020-06-05 22:08:29 -0300

    Refatore código components

--- web/src/App.js
+++ web/src/App.js
@@ -6,6 +6,8 @@ import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
+import  DevItem from './components/DevItem';
+
 function App() {
   const [devs, setDevs] = useState([]);
   
@@ -113,17 +115,7 @@ function App() {
     <main>
       <ul>
        {devs.map(dev => (
-        <li key={dev._id} className="dev-item">
-         <header>
-          <img src={dev.avatar_url} alt={dev.name}></img>
-          <div className="user-info">
-            <strong>{dev.name}</strong>
-            <span>{dev.techs.join(', ')}</span>
-          </div>
-        </header>
-        <p>{dev.bio}</p>
-        <a href={`https://github.com/${dev.github_username}`}>Acessar perfil no GitHub</a>
-      </li>
+         <DevItem key={dev._id} dev={dev} />
       ))}
     </ul>
   </main>
